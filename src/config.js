e75960a07b7954b24db777d709371b9294d53c66
Guilherme <bianchiniguilherme0@gmail.com>
2021-07-26 21:37:38 -0300

    Configurando o React

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "postitstore-app-upload",
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://v4u1hbaxw1.execute-api.us-east-2.amazonaws.com/prod",
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_YGWkoUcXw",
+      APP_CLIENT_ID: "2s6qbuob3gbhrfbkoc6onc6b4k",
+      IDENTITY_POOL_ID: "us-east-2:9d4e0267-1328-4790-8f80-ed0e4aa67953",
+    },
+  };
+  
+  export default config;
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Post-It: A Coisa</h1>
+        <p className="text-muted">Lembre-se do que mais te assusta!</p>
+      </div>
+    </div>
+  );
+}
